de37deebd383fd19219df514d47e7a23057b9951
Kurdalia <hwj930513@naver.com>
2022-09-08 13:59:20 +0900

    docs: Update example package

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,6 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:permission_request_page/permission_request_page.dart';
 
+// 1. Declare the required permissions in your app.
+const List<PermissionData> _kPermission = [
+  PermissionData(
+    permissionType: PermissionType.location,
+    description: 'Required to provide location services.',
+    isNecessary: true,
+  ),
+  PermissionData(
+    permissionType: PermissionType.storage,
+    description: 'Required to access the internal storage of the smartphone.',
+    isNecessary: false,
+  ),
+  PermissionData(
+    permissionType: PermissionType.notification,
+    description: 'Required to provide notifications.',
+    isNecessary: false,
+  ),
+];
+
 void main() => runApp(const ExampleApp());
 
 class ExampleApp extends StatelessWidget {
@@ -23,40 +42,28 @@ class SplashPage extends StatefulWidget {
 }
 
 class _SplashPageState extends State<SplashPage> {
+  // 2. Write an init function to execute when all required permissions are granted.
   Future<InitResult> _initFunction() async {
     InitResult initResult;
     try {
+      // Write your app initialization code.
       initResult = const InitResult(complete: true);
     } catch (error, stackTrace) {
+      // Write code to handle app initialization errors.
       initResult =
           InitResult(complete: false, error: error, stackTrace: stackTrace);
     }
 
+    // If the complete value of InitResult is true, it navigates to the nextPage.
     return initResult;
   }
 
   @override
   Widget build(BuildContext context) {
+    // 3. Add a PermissionRequestPage widget under Scaffold.
     return Scaffold(
       body: PermissionRequestPage(
-        permissions: const [
-          PermissionData(
-            permissionType: PermissionType.location,
-            description: 'Required to provide location services.',
-            isNecessary: true,
-          ),
-          PermissionData(
-            permissionType: PermissionType.storage,
-            description:
-                'Required to access the internal storage of the smartphone.',
-            isNecessary: false,
-          ),
-          PermissionData(
-            permissionType: PermissionType.notification,
-            description: 'Required to provide notifications.',
-            isNecessary: false,
-          ),
-        ],
+        permissions: _kPermission,
         initFunction: _initFunction,
         nextPage: Container(),
       ),
